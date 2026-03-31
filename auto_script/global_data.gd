301ee621bb4fb7a8749168aa86600ac6597c76a2
xiaoxuan <silasleon0251@gmail.com>
2026-03-31 19:51:30 +0800

    update global data
    完成场景节点的位置信息记录

--- auto_script/global_data.gd
+++ auto_script/global_data.gd
@@ -1,1 +1,13 @@
 extends Node
+
+# 开始界面
+const START_SCENE_PATH = "res://Scenes/UIScene/start_scene/StartScene.tscn"
+# 设置界面
+const SET_SCENE_PATH = "res://Scenes/UIScene/set_scene/SetScene.tscn"
+# 关卡界面
+const LEVEL_SCENE_PATH = "res://Scenes/SelectLevel/SelectLevel.tscn"
+
+# 关卡
+
+
+const L1_SCENE_PATH = "res://Scenes/GameScene/level_scene_one/Game1.tscn"
